92a6b4f707243b93b227e8603ace11c0a9b93bd8
WRIZZABLES <128195646+WRIZZABLES@users.noreply.github.com>
2023-03-20 22:08:36 -0400

    Delete wrizz for cookieclicker.js

--- wrizz for cookieclicker.js
+++
@@ -1,9 +0,0 @@
-let wantMaxCoins = prompt("Do you want max coins? (yes/no)"); // Prompt user for input
-if (wantMaxCoins === "yes") { // Check if user answered "yes"
-  // Run script in the console
-  console.log("Game.Earn(9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)"); // Example output to console
-} else if (wantMaxCoins === "no") { // Check if user answered "no"
-  console.log("No coins added."); // Example output to console
-} else { // Handle invalid input
-  console.log("Invalid input."); // Example error message to console
-}
