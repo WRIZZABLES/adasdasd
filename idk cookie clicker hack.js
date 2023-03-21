bb7cc28bfb7af9eb7d4738b6cd49051cc0d42941
WRIZZABLES <128195646+WRIZZABLES@users.noreply.github.com>
2023-03-20 21:59:55 -0400

    Delete idk cookie clicker hack.js

--- idk cookie clicker hack.js
+++
@@ -1,7 +0,0 @@
-let amount = prompt("How much do you want?"); // Prompt user for input
-if (amount) { // Check if user entered a value
-  // Run another script here
-  console.log("Game.Earn(+ amount +)"); // Example output to console
-} else {
-  console.log("No amount entered."); // Example error message to console
-}
