9295639c5eb59cb2e16c41208680679b3852f4b2
WRIZZABLES <128195646+WRIZZABLES@users.noreply.github.com>
2023-03-20 22:00:04 -0400

    Add files via upload

---
+++ idk cookie clicker hack.js
@@ -0,0 +1,7 @@
+let amount = prompt("How much do you want?"); // Prompt user for input
+if (amount) { // Check if user entered a value
+  // Run another script here
+  console.log("Game.Earn(amount)"); // Example output to console
+} else {
+  console.log("No amount entered."); // Example error message to console
+}
